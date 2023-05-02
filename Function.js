c916adc967c7b2c08bf2d64f1d41f0349473bbf8
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-05-02 23:49:23 +0600

    Function Done

---
+++ Function.js
@@ -0,0 +1,17 @@
+function myFunction(name) {
+  console.log(name + " is a good boy.");
+}
+myFunction("Hemel");
+
+function multiPlication(a, b) {
+  return a * b;
+}
+console.log(multiPlication);
+
+let carName = "Honda";
+function car() {
+  //let carName = "ToyoTa";
+  return console.log(carName);
+}
+car();
+console.log(carName);

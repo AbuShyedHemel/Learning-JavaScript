1f0766a7f58657168dd4fd11891015e8ce72e2f5
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-05-03 11:48:13 +0600

    CSS added

---
+++ JavaScript/Function.js
@@ -0,0 +1,22 @@
+function myFunction(name) {
+  console.log(name + " is a good boy.");
+}
+myFunction("Hemel");
+
+function multiPlication(a, b) {
+  return a * b;
+}
+console.log(multiPlication);
+
+let carName = "Honda";
+function car() {
+  //let carName = "ToyoTa";
+  return console.log(carName);
+}
+car();
+console.log(carName);
+
+function division(a,b){
+    return a / b;
+}
+division(4,2)

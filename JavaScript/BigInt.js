730b37d55ab30839db933049e3b28cf86ee04d2c
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-05-07 01:12:22 +0600

    NumberMethod,ArrayMethod,Array

---
+++ JavaScript/BigInt.js
@@ -0,0 +1,5 @@
+function BigInt(){
+    let x = 9999999999999999;
+    let y = BigInt("9999999999999999");
+document.getElementById("demo").innerHTML = a ;
+}
---
+++ JavaScript/NumberdMethod.js
@@ -0,0 +1,8 @@
+function NumberMethod(){
+    let x = 1.8668;
+    y = x.toString();
+    z = x.toExponential(2)
+    a = x.toFixed(4)
+    b = Number.parseInt(x)
+    document.getElementById("demo").innerHTML= b
+}

658e24da732ce02c93ebc76dabd0cbd9ee58b1c4
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-05-24 00:28:23 +0600

    Object Properties

---
+++ JavaScript/ObjectProperties.js
@@ -0,0 +1,19 @@
+const myObj = {
+    name: "John",
+    age: 30,
+    cars: [
+      {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+      {name:"BMW", models:["320", "X3", "X5"]},
+      {name:"Fiat", models:["500", "Panda"]}
+    ]
+  }
+
+  // get = " "
+
+  // for(let i in myObj.cars){
+  //   get +="<h1>" + myObj.cars[i].name + "</h1>"
+  //   for(let j in myObj.cars[i].models){
+  //       get +=myObj.cars[j].models
+  //   }
+  // }
+  console.log(x)

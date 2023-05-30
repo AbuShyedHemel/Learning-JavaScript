35d5a60f516d6300b365b186ec51391410ce476c
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-05-31 01:11:39 +0600

    Function Parameters

---
+++ JavaScript/FunctionParameter.js
@@ -0,0 +1,10 @@
+x = sumAll(1, 123, 500, 115, 44, 88);
+
+function sumAll() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+console.log(sumAll(x))
